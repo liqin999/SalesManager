1497caa04fab63354eeda8a7cc19357d608442a3
Your Name <liqin@300.cn>
2017-07-11 15:13:11 +0800

    数组排序，递归运算

--- src/views/bi/bi1.vue
+++ src/views/bi/bi1.vue
@@ -1,14 +1,55 @@
 <template>
-    <div class="mt50" style="font-size: 30px;color: gold">我是BI第一个内容</div>
+  <div>
+    <h2>过滤器排序功能：</h2>
+    {{message | capitalize }}
+    <hr/>
+    <input type="button" value="年龄升序" @click="sortFn(0)" style="margin-left: 20px;margin-bottom: 20px;"/>
+    <input type="button" value="年龄降序" @click="sortFn(1)" style="margin-left: 20px;margin-bottom: 20px;"/>
+    <input type="button" value="新增" @click="addFn" style="margin-left: 20px;margin-bottom: 20px;"/>
+    <table border="1" style="margin: 0 auto">
+      <thead>
+      <tr>
+        <th style="cursor: pointer;">名字</th>
+        <th style="cursor: pointer">年龄</th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr v-for="item in dataList">
+        <td v-for="(val, key, index) in item">{{val}}</td>
+      </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
-
 <script>
-
-
   //监听到路由发生呢变化的时候，让页面可以重新点击
   export default {
     data: function () {
       return {
+        message:"hello",
+        dataList1:[],
+        dataList:[
+          {
+            name:"liqin",
+            age:12
+          },
+          {
+            name:"zhangsan",
+            age:10
+          },
+          {
+            name:"aaaa",
+            age:1
+          },
+          {
+            name:"zhaoliu1",
+            age:33
+          },
+          {
+            name:"zhaoliu2",
+            age:13
+          }
+        ]
       }
     },
     watch:{
@@ -19,7 +60,49 @@
     created: function () {
     },
     methods:{
-    }
+      addFn:function(){
+        this.dataList.push({
+          name:"李白",
+          age:22
+        })
+      },
+      sortFn: function (flag) {
+        var temp={};
+        if(flag == 0){
+          temp = this.dataList[0];
+          for(var i=1;i<this.dataList.length;i++){
+            if(this.dataList[i].age < this.dataList[i-1].age){
+              temp = this.dataList[i-1];
+              this.dataList[i-1] = this.dataList[i];
+              this.dataList[i] = temp;
+              this.sortFn(0);//每次执行之后，再次调用这个方法，执行函数，递归
+            }else{
+              temp=this.dataList[i-1]
+            }
+          }
+        }else if(flag == 1){//将temp保存最大值
+          temp = this.dataList[0];
+          for(var i=1;i<this.dataList.length;i++){
+            if(this.dataList[i].age > this.dataList[i-1].age){
+              temp = this.dataList[i];
+              this.dataList[i] = this.dataList[i-1];
+              this.dataList[i-1] = temp;
+              this.sortFn(1)
+            }else{
+              temp=this.dataList[i-1];
+            };
+          }
+        }
+        this.dataList = this.dataList.splice(0);//解决在vue数组中，数据更新后，视图不更新
+        temp={};//每次执行排序后，将临时的对象清空
+      }
+    },
+      filters:{
+    capitalize: function (value) {
+      value = value.toString();//toString() 方法可把一个逻辑值转换为字符串，并返回结果。   toString  将逻辑值转化成一个字符串的形式  charAt()找到字符串中某个位置的字符
+      return value.charAt(0).toLocaleUpperCase() + value.slice(1)
+       }
+     }
   }
 
 </script>
---
+++ src/views/order/order3.vue
@@ -0,0 +1,407 @@
+<template>
+  <div class="container body-content">
+    <div id="test" class="form-group">
+      <div class="form-group">
+        <div class="page-header">
+          <span style="color: cyan;font-size:35px;;">利用vuex状态管理后台表格数据:{{getNum}}</span>
+        </div>
+        <form class="form-inline" style="margin-bottom: 30px;">
+          <div class="form-group">
+            <input type="text" placeholder="姓名" class="form-control" v-model="searchname"/>
+            <button type="button" class="btn btn-primary">搜索</button>
+          </div>
+          <div class="form-group">
+            <button type="button" class="btn btn-danger" @click="resetData()">重置</button>
+          </div>
+          <div class="form-group">
+            <button type="button" class="btn btn-primary" data-toggle="modal"  @click="showModal()">
+              新增
+            </button>
+          </div>
+        </form>
+        <table class="table table-bordered table-responsive table-striped table-hover">
+          <thead>
+          <tr class="text-center">
+            <th class="text-center">姓名</th>
+            <th class="text-center">年龄</th>
+            <th class="text-center">删除信息</th>
+          </tr>
+          </thead>
+          <tbody>
+          <!--<tr v-for="(item,index) in filterArrayData">-->
+          <tr v-for="(item,index) in arrayData">
+            <td width="40%" class="text-center">{{item.name}}</td>
+            <td width="30%">{{item.age}}</td>
+            <td>
+              <a href="javascript:void(0)">
+                <button class="btn btn-primary btn-sm" @click="showModalEdit(index,item.name,item.age)" data-toggle="modal">编辑</button>
+                <button class="btn btn-warning btn-sm" @click="showModalDel(index,item.age)" data-toggle="modal">删除</button>
+              </a>
+            </td>
+          </tr>
+          </tbody>
+        </table>
+        <div class="pager" id="pager">
+          <!--开头的页码-->
+          <span class="form-inline">
+              <select class="form-control" v-model="pagesize" @change="showPage(pageCurrent,$event,true)" number>
+                <option value="10">10</option>
+                <option value="20">20</option>
+                <option value="30">30</option>
+                <option value="40">40</option>
+              </select>
+          </span>
+          <!--中间页码-->
+          <template v-for="item in pageCount+1">
+                        <span v-if="item==1" class="btn btn-default" v-on:click="showPage(1,$event)">
+                            首页
+                        </span>
+                        <span v-if="item==1" class="btn btn-default" v-on:click="showPage(pageCurrent-1,$event)">
+                            上一页
+                        </span>
+                        <span v-if="item==1" class="btn btn-default" v-on:click="showPage(item,$event)">
+                            {{item}}
+                        </span>
+                        <span v-if="item==1&&item<showPagesStart-1" class="btn btn-default disabled">
+                            ...
+                        </span>
+                        <span v-if="item>1&&item<=pageCount-1&&item>=showPagesStart&&item<=showPageEnd&&item<=pageCount" class="btn btn-default" v-on:click="showPage(item,$event)">
+                            {{item}}
+                        </span>
+                        <span v-if="item==pageCount&&item>showPageEnd+1" class="btn btn-default disabled">
+                            ...
+                        </span>
+                        <span v-if="item==pageCount" class="btn btn-default" v-on:click="showPage(item,$event)">
+                            {{item}}
+                        </span>
+                        <span v-if="item==pageCount" class="btn btn-default" v-on:click="showPage(pageCurrent+1,$event)">
+                            下一页
+                        </span>
+                        <span v-if="item==pageCount" class="btn btn-default" v-on:click="showPage(pageCount,$event)">
+                            尾页
+                        </span>
+          </template>
+
+          <!--尾部-->
+           <span class="form-inline">
+                <input class="pageIndex form-control"
+                       style="width:60px;text-align:center"
+                       type="text"
+                       v-model="pageCurrent"
+                       @keyup.enter="showPage(pageCurrent,$event,true)"
+                  />
+               </span>
+          <span>{{pageCurrent}}/{{pageCount}}</span>
+        </div>
+      </div>
+    </div>
+    <!-- Add Modal -->
+    <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+            <h4 class="modal-title" id="myModalLabel">新增菜单</h4>
+          </div>
+          <div class="modal-body">
+            <form>
+              <div class="form-group text-left">
+                <label for="recipient-name" class="control-label">姓名:</label>
+                <input type="text" class="form-control" id="recipient-name"  v-model="addName">
+              </div>
+              <div class="form-group text-left">
+                <label for="recipient-age" class="control-label" >年龄:</label>
+                <input type="text" class="form-control" id="recipient-age" v-model="addAge">
+              </div>
+
+            </form>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-default" data-dismiss="modal">关闭</button>
+            <button type="button" class="btn btn-primary" @click="saveAddData()">保存</button>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!--edit Model 组件方式引入-->
+
+    <editModal v-if="isShowEdit" mess="EditModal" :EditingNam="EditingName" :EditingAg="EditingAge"  :EditingInde="EditingIndex" v-on:editMsg="handleEdit" v-on:closeEditModal="handleClose"></editModal>
+
+    <!--Delete Modal-->
+    <div role="dialog" class="modal fade bs-example-modal-sm" id="layer">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal">
+              <span>&times;</span>
+            </button>
+            <h4 class="modal-title">确认删除么?</h4>
+          </div>
+          <div class="modal-body text-right">
+            <button data-dismiss="modal" class="btn btn-primary btn-sm">取消</button>
+            <button data-dismiss="modal" class="btn btn-danger btn-sm" @click="deleteMsg(nowIndex)">确认</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  </div>
+</template>
+<script>
+  //引入状态管理
+  import { mapState,mapGetters,mapActions,mapMutations} from 'vuex'//在组件中使用Mutations
+  import editModal from './editModal.vue'
+  var arrayData=[
+    {
+      "name":"zhangsan",
+      "age":12
+    },
+    {
+      "name":"liqin0",
+      "age":12
+    },
+    {
+      "name":"liqin1",
+      "age":12
+    },
+    {
+      "name":"liqin2",
+      "age":12
+    }
+    ,
+    {
+      "name":"liqin3",
+      "age":12
+    },
+    {
+      "name":"liqin4",
+      "age":12
+    },
+    {
+      "name":"liqin5",
+      "age":12
+    },
+    {
+      "name":"liqin6",
+      "age":12
+    },
+    {
+      "name":"liqin7",
+      "age":12
+    },
+    {
+      "name":"liqin8",
+      "age":12
+    },
+    {
+      "name":"liqin9",
+      "age":11
+    },
+    {
+      "name":"wang",
+      "age":20
+    }
+  ];
+  export default {
+    data: function () {
+      return {
+        //总项目数
+        totalCount: 200,
+        //分页数
+        pageCount: 20,
+        //当前页面
+        pageCurrent: 1,
+        //分页大小
+        pagesize: 10,
+        //显示分页按钮数
+        showPages: 5,
+        //开始显示的分页按钮
+        showPagesStart: 1,
+        //结束显示的分页按钮
+        showPageEnd: 100,
+        //分页数据
+        arrayData: arrayData,
+
+        //搜索的项
+        searchname:"",
+        searchage:"",
+
+        //新增用户信息
+        addName:"",
+        addAge:"",
+
+        // 删除和编辑用户记录索引
+        nowIndex:-100,
+
+        //定义一些临时的变量用来存取编辑的内容
+        EditingIndex:-100,
+        EditingName:"",
+        EditingAge:"",
+
+        //模拟分页中的数据
+        newPageInfo:[],
+
+        //弹框默认隐藏
+        isShowEdit:false
+      }
+    },
+    components: {
+      editModal:editModal
+    },
+    computed: {//计算属性
+      getNum: function () {
+        return this.$store.state.count
+      },
+      filterArrayData: function () {
+        var self = this;
+        return self.arrayData.filter(function (item) {
+          return item.name.indexOf(self.searchname) !== -1
+        })
+      }
+    },
+    watch: {
+      $route: function () {
+
+      }
+    },
+    created: function () {
+      this.showPage(this.pageCurrent, null, true);
+    },
+    methods: {
+      handleClose: function () {//关闭对话框隐藏当前的模块
+        $('#EditModal').modal('hide');
+        this.isShowEdit=false;
+      },
+      handleEdit: function () {
+        var newName=arguments[0];
+        var newAge=arguments[1];
+        var this_index=arguments[2];
+        //修改表单中的对应索引的值  将从编辑修改项取到的值，渲染到页面
+        this.arrayData[this_index].name=newName;
+        this.arrayData[this_index].age=newAge;
+        $('#EditModal').modal('hide');
+        this.isShowEdit=false;
+      },
+      showModalDel:function(index){//删除用户
+        $('#layer').modal('show');
+        this.nowIndex=index;
+      },
+      showModalEdit: function (_index,name,age) {//编辑用户
+        this.isShowEdit=true;
+        this.EditingName=name;
+        this.EditingAge=age;
+        this.EditingIndex=_index;
+        window.setTimeout(function () {
+          $('#EditModal').modal('show');
+        },200);
+      },
+      deleteMsg: function (index) {
+        this.arrayData.splice(index,1);
+      },
+      showModal: function () {//新增用户  向数组中push 新增加一条数据 this是当前对象的实例
+        this.addName="";//新增的时候表单置为空
+        this.addAge="";
+        $('#myModal').modal('show');
+      },
+      saveAddData: function () {//保存新增的用户信息
+        if(this.addName && this.addAge){
+          console.log(this.addName+","+this.addAge);//构造函数this代指当前对象的实例
+          this.arrayData.push({
+            "name":this.addName,
+            "age":this.addAge
+          })
+        }
+        $('#myModal').modal('hide');
+      },
+      resetData: function () {
+        //找到对应的文本框，让里面的的输入的值，清空
+        this.searchname="";//将数据清空，由于数据驱动视图实时的更新，所以恢复到原始的数据
+      },
+      showPage: function (pageIndex, $event, forceRefresh) {//显示分页的代码
+        var This=this;
+        if (pageIndex > 0) {
+          if (pageIndex > this.pageCount) {
+            pageIndex = this.pageCount;
+          }
+          //判断数据是否需要更新
+          var currentPageCount = Math.ceil(this.totalCount / this.pagesize);
+          if (currentPageCount != this.pageCount) {
+            pageIndex = 1;
+            this.pageCount = currentPageCount;
+          }
+          else if (this.pageCurrent == pageIndex && currentPageCount == this.pageCount && typeof (forceRefresh) == "undefined") {
+            console.log("not refresh");
+            return;
+          }
+          //处理分页点中样式
+          var buttons = $("#pager").find("span");
+          for (var i = 0; i < buttons.length; i++) {
+            if (buttons.eq(i).html() != pageIndex) {
+              buttons.eq(i).removeClass("active");
+            }
+            else {
+              buttons.eq(i).addClass("active");
+            }
+          }
+
+          //测试数据 随机生成的
+          var newPageInfo = [];
+          for (var i = 0; i < this.pagesize; i++) {
+            newPageInfo[newPageInfo.length] = {
+              name: "test" + (i + (pageIndex - 1) * 20),
+              age: (i + (pageIndex - 1) * 20)
+            };
+          }
+          var that=this;
+          function getData(){//动态获取数据  slice可以控制显示的条数
+            that.$http.get('../js/data.json',{
+              pageIndex:pageIndex,
+              pagesize:This.pagesize
+            }).then(function(res){
+              newPageInfo = newPageInfo.concat(res.data);
+              console.log(newPageInfo);//将请求的数据存放到提前设置的数组中
+              This.pageCurrent = pageIndex;
+              This.arrayData = newPageInfo;
+            },function(res){
+              alert(res.status);
+            });
+          }
+          //getData();
+
+          this.pageCurrent = pageIndex;
+          //this.arrayData = newPageInfo;
+
+          //计算分页按钮数据 20 5
+          if (this.pageCount > this.showPages) {
+            if (pageIndex <= (this.showPages - 1) / 2) {
+              this.showPagesStart = 1;
+              this.showPageEnd = this.showPages - 1;
+              console.log("showPage1")
+            }
+            else if (pageIndex >= this.pageCount - (this.showPages - 3) / 2) {
+              this.showPagesStart = this.pageCount - this.showPages + 2;
+              this.showPageEnd = this.pageCount;
+              console.log("showPage2");
+            }
+            else {
+              console.log("showPage3");
+              this.showPagesStart = pageIndex - (this.showPages - 3) / 2;
+              if(!isNaN(pageIndex)){//用于处理用户输入的不是数字的处理
+
+              }else{
+
+              }
+              this.showPageEnd = Number(pageIndex) + (Number(this.showPages) - 3) / 2;//将输入的值强制转成数字
+            }
+          }
+          console.log("showPagesStart:" + this.showPagesStart + ",showPageEnd:" + this.showPageEnd + ",pageIndex:" + pageIndex);
+        }
+      }
+    }
+
+  }
+
+</script>
+<style>
+</style>
