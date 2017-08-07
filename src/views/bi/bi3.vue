63b34280c69074c0c6f27ab9e646e803e12ec5d3
Your Name <liqin@300.cn>
2017-08-07 14:07:49 +0800

    使用es6的模块语法引入校验的选项

--- src/views/bi/bi3.vue
+++ src/views/bi/bi3.vue
@@ -3,7 +3,6 @@
       <div class="mt50" style="font-size: 30px;color:black">
          前端技能组件:
       </div>
-
       <input type="text" placeholder="请选择" v-model='getVal'>
 
       <drop :data='sportList' v-on:getData="handleData">
@@ -14,17 +13,30 @@
           我是底部的插槽
         </div>
       </drop>
-
+        <br>
+        <label for="">邮件：</label>
+        <input type="text" 
+         v-model="email" 
+        :class="{borderRed:isEmailShow}" 
+         placeholder="输入邮件" 
+         @input='checkInput()'>
+        <div style='color:red' v-show="isEmailShow">
+          {{emailTip}}
+        </div>
     </div>
 </template>
 <script>
   //监听到路由发生呢变化的时候，让页面可以重新点击
-  // 引入组价并调用
+  // 引入组价并调用  页面中资源的共享属性 export 
   import drop from "@/components/dropDown"
+  import {Regexs} from "@/utils/index"
   export default {
     data: function () {
       return {
          getVal:'',
+         email:'',
+         emailTip:'',
+         isEmailShow:false,//一个数据项控制两个显示的视图的地方
          sportList:[
          {
           name:'javascript'
@@ -39,6 +51,20 @@
       drop,
     },
     methods:{
+      checkInput(){
+        if(this.email == ""){
+           this.isEmailShow = true;
+          this.emailTip = "输入不能为空";
+        } else if(Regexs.Email.test(this.email)){
+          this.isEmailShow = false;
+          this.emailTip = "恭喜你输入正确的格式~~~"
+          return true;
+        }else{
+          this.isEmailShow = true;
+          this.emailTip = "格式不正确，重新输入邮箱号";
+          return false;//结束执行
+        }
+      },
       handleData(val){//接受到自组件传递的数据，然后覆盖到页面上
         // alert(val)
         this.getVal = val;
@@ -48,4 +74,5 @@
 
 </script>
 <style>
+.borderRed{border:1px solid red;}
 </style>
