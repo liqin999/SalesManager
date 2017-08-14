9a01b536fbb6fc5252d17e578f359e9d5192f34b
Your Name <liqin@300.cn>
2017-08-14 17:29:58 +0800

    增加地址三级联动的插件

--- src/views/bs/bs1.vue
+++ src/views/bs/bs1.vue
@@ -1,15 +1,31 @@
 <template>
-  <div class="mt50" style="font-size: 30px;color: blue">我是BS第一个内容</div>
+<div>
+  <div class="mt50" style="font-size: 30px;color: blue">导入地址插件</div>
 
+  <el-form :inline="true" :model="formCreate" class="demo-form-inline" label-position="right" label-width="120px">
+     <el-form-item label="地区：" prop="provinceOptions">
+       <el-cascader 
+       :options="provinceOptions" 
+       v-model="formCreate.provinceOptions"
+        @change="ChangeRegions"
+        ></el-cascader>
+     </el-form-item>
+    </el-form>
+</div>
 </template>
 
 <script>
-
-
-  //监听到路由发生呢变化的时候，让页面可以重新点击
+import citys from '@/utils/citys.js'
   export default {
+    mounted(){
+        console.log(citys);
+    },
     data: function () {
       return {
+        provinceOptions: citys.regions,//将下拉的数据显示出来
+        formCreate:{
+          provinceOptions: [],
+        }
       }
     },
     watch:{
@@ -20,6 +36,9 @@
     created: function () {
     },
     methods:{
+      ChangeRegions(){
+        console.log(this.formCreate.provinceOptions);//可以将选择的数据传递到后台
+      }
     }
   }
 
