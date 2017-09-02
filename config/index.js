df3cf6c5acf3d47068fb00fa86bfd3e0b4d43622
liqin <1483510623@qq.com>
2017-09-02 09:24:54 +0800

    上线配置

--- config/index.js
+++ config/index.js
@@ -7,7 +7,7 @@ module.exports = {
     index: path.resolve(__dirname, '../dist/index.html'),
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/dist/',//配置服务器上的路径 www/dist/static|index.html
     productionSourceMap: true,
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
